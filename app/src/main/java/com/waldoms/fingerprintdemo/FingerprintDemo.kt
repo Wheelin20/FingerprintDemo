edd49cc951ff751ff2c9eb62f07321ee5ffeef8c
Kevin Emigh <kevin.emigh@ohiohealth.com>
2018-11-15 13:38:36 -0500

    initial commit

---
+++ app/src/main/java/com/waldoms/fingerprintdemo/FingerprintDemo.kt
@@ -0,0 +1,12 @@
+package com.waldoms.fingerprintdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FingerprintDemo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fingerprint_demo)
+    }
+}
